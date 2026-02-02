79cfe2ee57203778d07a971bb13881e124d89ec2
QueenDekim <queendekim.twitch@gmail.com>
2026-02-03 00:47:37 +0300

    Filter tools in admin to show only those with registered modules

--- core/admin.py
+++ core/admin.py
@@ -20,3 +20,9 @@ class ToolAdmin(admin.ModelAdmin):
     list_display = ['name', 'status', 'version', 'last_updated']
     list_filter = ['status']
     search_fields = ['name']
+
+    def get_queryset(self, request):
+        from .plugin_system import plugin_registry
+        qs = super().get_queryset(request)
+        registered_module_ids = [m.module_id for m in plugin_registry.get_all_modules()]
+        return qs.filter(name__in=registered_module_ids)
